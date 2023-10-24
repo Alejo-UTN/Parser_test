4fa1fed6a5f79f2a37a42736e38df449ceb5a21a
Alejo E. Garcia Mattos <128099878+Alejo-UTN@users.noreply.github.com>
2023-10-24 14:25:53 -0300

    Update parser_tp1.py

--- parser_tp1.py
+++ parser_tp1.py
@@ -6,3 +6,29 @@ def traduccionParser(salidaLexer):
         cadena.append(tupla[0])
     cadena.append('#')
     return cadena
+
+def genDerivaciones(topePila, prodAnterior, derivacion)
+    #wea para saber la posicion del elemento a derivar
+    indice = prodAnterior.index(topePila)
+    #limpio el elemento a derivar
+    prodAnterior.remove(topePila)
+
+
+# "Program -> Estructura Program" tiene como simbolos directrices a: "ENTONCES", "SINO", "FINSI", "REPETIR", "HASTA", "SI"...
+# El EOF es el simbolo #
+
+# tabla[Columna][Fila]
+tabla = {
+    "program": {
+        "ENTONCES" :     [ "estructura" , "program"],
+        "SINO" :         [ "estructura" , "program"],
+        "FINSI" :        [ "estructura" , "program"],
+        "REPERTIR" :     [ "estructura" , "program"],
+        "HASTA" :        [ "estructura" , "program"],
+        "SI" :           [ "estructura" , "program"],
+        "LEER" :         [ "estructura" , "program"],
+        "FUN" :          [ "estructura" , "program"],
+
+
+
+VT = ["ENTONCES", "SINO", "FINSI", "REPETIR", "HASTA", "SI", "+", "*", "LEER", "MOSTAR", "FUN", "FINFUC", "=", "," , ";", ">,<,<=,>=", "NUMERO", "ID"]
