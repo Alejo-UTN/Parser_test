1147aa2f0bf3c97fdb638a34780b771efa63fea9
Alejo E. Garcia Mattos <128099878+Alejo-UTN@users.noreply.github.com>
2023-10-24 13:33:38 -0300

    Update parser_tp1.py

--- parser_tp1.py
+++ parser_tp1.py
@@ -1,1 +1,8 @@
-
+# Función que convierte la salida del lexer a una lista que el parser puede entender
+def traduccionParser(salidaLexer):
+    cadena = []
+    # Ponemos cada primer elemento de cada tupla (token) en una lista
+    for tupla in salidaLexer:
+        cadena.append(tupla[0])
+    cadena.append('#')
+    return cadena
