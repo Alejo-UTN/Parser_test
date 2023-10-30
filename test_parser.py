a58160dd6ce6536d1676d51c2f27cdba8ee3e3ef
Alejo E. Garcia Mattos <128099878+Alejo-UTN@users.noreply.github.com>
2023-10-30 12:05:46 -0300

    Update test_parser.py

--- test_parser.py
+++ test_parser.py
@@ -2,13 +2,14 @@ from Lexer_tp.py import lexer
 from parser_tp1.py import parser
 
 cadenaPrueba = [
+  " SI var < var  ENTONCES FUN , SINO FUN ",
+  " MOSTRAR var",
+  " LEER var",
+  " MOSTRAR x + 9",
+  " ID = ID ",
+  " REPETIR FUN 2+2 HASTA 4 "
   "
-
-
-
-
-
-
+  
 ]
 i = 1
 for ejemplo in cadenaPrueba:
