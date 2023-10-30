8108ac61d409790020bb52a8d12f58699c251f9d
Alejo E. Garcia Mattos <128099878+Alejo-UTN@users.noreply.github.com>
2023-10-30 11:28:44 -0300

    Update test_parser.py

--- test_parser.py
+++ test_parser.py
@@ -1,2 +1,17 @@
 from Lexer_tp.py import lexer
-from parser_tp1.py import
+from parser_tp1.py import parser
+
+cadenaPrueba = [
+  "
+
+
+
+
+
+
+]
+i = 1
+for ejemplo in cadenaPrueba:
+  print ("=============Cadena N°", i, "=============")
+  parser(traduccionParser(lexer(ejemplo)))
+  i += 1
