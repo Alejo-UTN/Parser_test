b47dcb4baeb30567e654192bb0738e951195e899
Alejo E. Garcia Mattos <128099878+Alejo-UTN@users.noreply.github.com>
2023-11-26 08:06:56 -0300

    Update test_parser.py

--- test_parser.py
+++ test_parser.py
@@ -1,17 +1,18 @@
-from Lexer_tp.py import lexer
-from parser_tp1.py import parser
+from Lexer_tp import lexer
+from Parser_tp1_v2 import traduccionParser, parser
 
-cadenaPrueba = [
-  " SI var < var  ENTONCES FUN , SINO FUN ",
-  " MOSTRAR var",
-  " LEER var",
-  " MOSTRAR x + 9",
-  " ID = ID ",
-  " REPETIR FUN 2+2 HASTA 4 ",
-  " x = 70",
-  " MOSTRAR x * 10",
-  "SI var > var ENTONCES FUN, SINO FUN"
-]
+# Pruebas:
+parser(traduccionParser(lexer("leer var")))
+parser(traduccionParser(lexer("aux equal 5")))
+parser(traduccionParser(lexer("func aux (aux2) leer aux3 finfunc")))
+parser(traduccionParser(lexer("aux equal 9")))
+parser(traduccionParser(lexer("mostrar 5")))
+parser(traduccionParser(lexer("repetir mostrar 3 hasta aux3")))
+parser(traduccionParser(lexer("si 5>5 entonces mostrar x+6 finsi")))
+parser(traduccionParser(lexer("mostrar x+3")))
+parser(traduccionParser(lexer("func rest(n1; n2) x equal n1 - n2; mostrar x finfunc")))
+parser(traduccionParser(lexer("mostrar x + 5")))
+
 i = 1
 for ejemplo in cadenaPrueba:
   print ("=============Cadena N°", i, "=============")
