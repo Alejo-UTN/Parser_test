f81adfdd5c2cb7e70a238a420f571081c23e66eb
Alejo E. Garcia Mattos <128099878+Alejo-UTN@users.noreply.github.com>
2023-11-27 21:49:00 -0300

    Update test_parser.py

--- test_parser.py
+++ test_parser.py
@@ -1,17 +1,10 @@
 from Lexer_tp import lexer
 from Parser_tp1_v2 import traduccionParser, parser
 
-# Pruebas:
-parser(traduccionParser(lexer("leer var")))
-parser(traduccionParser(lexer("aux equal 5")))
-parser(traduccionParser(lexer("func aux (aux2) leer aux3 finfunc")))
-parser(traduccionParser(lexer("aux equal 9")))
-parser(traduccionParser(lexer("mostrar 5")))
-parser(traduccionParser(lexer("repetir mostrar 3 hasta aux3")))
-parser(traduccionParser(lexer("si 5>5 entonces mostrar x+6 finsi")))
-parser(traduccionParser(lexer("mostrar x+3")))
-parser(traduccionParser(lexer("leer variable")))
-parser(traduccionParser(lexer("mostrar x + 5")))
+cadenasEjemplo = [
+    "L",
+    "MOSTRAR (5)"
+]
 
 i = 1
 for ejemplo in cadenaPrueba:
